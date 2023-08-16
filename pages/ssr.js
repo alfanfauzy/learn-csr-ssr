0a0b26b0ed63eb4a9a563ebd36c874cd240ad2eb
alfanfauzy <54702969+alfanfauzy@users.noreply.github.com>
2023-08-17 00:35:20 +0700

    Change Api Endpoint

--- pages/ssr.js
+++ pages/ssr.js
@@ -4,18 +4,30 @@ const SSRPage = ({ data }) => {
   return (
     <Layout>
       <h1>You here SSR Page</h1>
-      {data && data.map((user) => <p key={user.id}>{user.name}</p>)}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 10,
+        }}
+      >
+        {data && data.map((cat) => <img src={cat.url} width={120} />)}
+      </div>
     </Layout>
   );
 };
 
 export const getServerSideProps = async () => {
   console.log('getServerSideProps is running.');
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const dataUsers = await response.json();
+  const response = await fetch(
+    'https://api.thecatapi.com/v1/images/search?limit=10'
+  );
+  const dataCats = await response.json();
 
   return {
-    props: { data: dataUsers },
+    props: { data: dataCats },
   };
 };
 export default SSRPage;
